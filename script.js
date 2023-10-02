5417b87fb5062006d8acdb96e4ac9bd8b567bd34
rayyannkhan <141767590+rayyannkhan@users.noreply.github.com>
2023-10-02 23:01:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var a, b;
+a = prompt("Enter number");
+b = prompt("Enter 2nd number");
+c = a * b;
+d = Number(a) + Number(b);
+e = a - b;
+f = a / b; 
+g = a % b;
+
+function mul() {
+  alert("The product of the numbers u entered is " + c);
+}
+
+function add() {
+  alert("The sum of the numbers u entered is " + d );
+}
+
+function sub() {
+  alert("The subtraction of the numbers u entered is " + e);
+}
+
+function div() {
+  alert("The dividion of the numbers u entered is " + f);
+}
+
+function perc() {
+  alert("The % of the numbers u entered is " + g);
+}
